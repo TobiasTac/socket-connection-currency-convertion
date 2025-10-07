7a844f2a0c5ddcb6abd5c1fbc4cfe14d6eefb6fc
TobiasTac <tobias.vida@live.com>
2025-10-07 10:09:13 -0300

    refactor: accept only one connection

--- server/server.go
+++ server/server.go
@@ -19,41 +19,6 @@ func classifyValue(valueUSD float64) string {
 	}
 }
 
-func handleConnection(conn net.Conn) {
-	fmt.Println("Client Connect:", conn.RemoteAddr())
-	defer conn.Close()
-
-	for {
-		reader := bufio.NewReader(conn)
-		msg, err := reader.ReadString('\n')
-
-		if err != nil {
-			fmt.Println("Client disconnected or read error:", err)
-			return
-		}
-
-		fmt.Printf("Message Received (BRL): %s", msg)
-
-		trimmed := strings.TrimSpace(msg)
-
-		valueBRL, err := strconv.ParseFloat(trimmed, 64)
-
-		if err != nil || valueBRL < 0 {
-			errMsg := "Error: You typed wrong\n"
-			conn.Write([]byte(errMsg))
-			continue
-		}
-
-		valueUSD := valueBRL * exchangeRate
-
-		classification := classifyValue(valueUSD)
-
-		response := fmt.Sprintf("BRL: R$%.2f. Converted to USD: $%.2f. Status: %s\n", valueBRL, valueUSD, classification)
-
-		conn.Write([]byte(response))
-	}
-}
-
 func main() {
 	listener, err := net.Listen("tcp", "127.0.0.1:3000")
 
@@ -65,13 +30,39 @@ func main() {
 	fmt.Println("🔥 Server started at localhost:3000")
 	defer listener.Close()
 
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection", err)
+		return
+	}
+
+	fmt.Println("Client accepted.")
+	defer conn.Close()
+
 	for {
-		conn, err := listener.Accept()
+		reader := bufio.NewReader(conn)
+		msg, err := reader.ReadString('\n')
+
 		if err != nil {
-			fmt.Println("Error accepting connection", err)
+			fmt.Println("Client disconnected or read error:", err)
+			break
+		}
+
+		fmt.Printf("Message Received (BRL): %s", msg)
+
+		trimmed := strings.TrimSpace(msg)
+		valueBRL, err := strconv.ParseFloat(trimmed, 64)
+
+		if err != nil || valueBRL < 0 {
+			errMsg := "Error: You typed wrong\n"
+			conn.Write([]byte(errMsg))
 			continue
 		}
 
-		go handleConnection(conn)
+		valueUSD := valueBRL * exchangeRate
+		classification := classifyValue(valueUSD)
+
+		response := fmt.Sprintf("BRL: R$%.2f. Converted to USD: $%.2f. Status: %s\n", valueBRL, valueUSD, classification)
+		conn.Write([]byte(response))
 	}
 }
